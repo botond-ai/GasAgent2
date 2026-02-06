8e75b3db508abbb0d43bf90ab48e1f46bdc0bf68
Botond Kovacs <botond.kovacs@crystalclear.hu>
2026-02-06 18:18:29 +0100

    Make S3 bucket and DynamoDB table creation idempotent in bootstrap

--- terraform-bootstrap/main.tf
+++ terraform-bootstrap/main.tf
@@ -1,9 +1,23 @@
+data "aws_s3_bucket" "existing" {
+  bucket = "terraform-state-640706953781-gasagent2"
+  count  = 0 # always 0, just for reference
+}
+
 resource "aws_s3_bucket" "tf_state" {
   bucket = "terraform-state-640706953781-gasagent2"
   force_destroy = true
   tags = {
     Name = "tf-state-bucket"
   }
+  lifecycle {
+    prevent_destroy = true
+    ignore_changes = [tags]
+  }
+}
+
+data "aws_dynamodb_table" "existing" {
+  name  = "terraform-state-lock"
+  count = 0 # always 0, just for reference
 }
 
 resource "aws_dynamodb_table" "tf_lock" {
@@ -17,5 +31,9 @@ resource "aws_dynamodb_table" "tf_lock" {
   }
   tags = {
     Name = "tf-state-lock"
+  }
+  lifecycle {
+    prevent_destroy = true
+    ignore_changes = [tags]
   }
 }
