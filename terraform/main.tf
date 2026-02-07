5a557bb240583407b8887621b5808871b3c65ba9
Botond Kovacs <botond.kovacs@crystalclear.hu>
2026-02-07 16:21:41 +0100

    Replace data source with resource for ALB listener and associate target group

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,9 +19,14 @@ data "aws_lb" "main" {
 }
 
 
-data "aws_lb_listener" "main" {
+resource "aws_lb_listener" "main" {
   load_balancer_arn = data.aws_lb.main.arn
   port              = 80
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
 }
 module "bootstrap" {
   source = "../terraform-bootstrap"
