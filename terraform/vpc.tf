769a751d0777051cf239c5ce601af673fbbb4d81
Botond Kovacs <botond.kovacs@crystalclear.hu>
2026-02-09 10:03:21 +0100

    Fix ECS tasks security group to allow ALB on port 80

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -118,8 +118,8 @@ resource "aws_security_group" "ecs_tasks" {
 
   # Bejövő forgalom csak az ALB-től
   ingress {
-    from_port       = 8000
-    to_port         = 8000
+    from_port       = 80
+    to_port         = 80
     protocol        = "tcp"
     security_groups = [aws_security_group.alb.id]
     description     = "Allow traffic from ALB"
